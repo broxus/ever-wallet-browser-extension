84b6de18997ae275f9a4759a6942130181040845
Egor Komarov <egor.komarov@bf.rocks>
2023-03-30 17:56:27 +0200

    feat: minor design updates

--- src/popup/modules/main/components/ChangeAccount/AccountItem.tsx
+++ src/popup/modules/main/components/ChangeAccount/AccountItem.tsx
@@ -1,30 +1,36 @@
-import { memo } from 'react'
+import { memo, useCallback, useState } from 'react'
 
 import { convertAddress } from '@app/shared'
-import { UserAvatar } from '@app/popup/modules/shared'
+import { Spinner, UserAvatar } from '@app/popup/modules/shared'
 
 interface Props {
     address: string;
     name: string;
     seed: string;
-    onClick(address: string): void;
+    onClick(address: string): Promise<void>;
 }
 
-export const AccountItem = memo(({ address, name, seed, onClick }: Props): JSX.Element => (
-    <div
-        className="change-account__account"
-        onClick={() => onClick(address)}
-    >
-        <UserAvatar className="change-account__account-avatar" address={address} small />
-        <div className="change-account__account-content">
-            <div className="change-account__account-name" title={name}>
-                {name}
+export const AccountItem = memo(({ address, name, seed, onClick }: Props): JSX.Element => {
+    const [loading, setLoading] = useState(false)
+    const handleClick = useCallback(() => {
+        setLoading(true)
+        onClick(address).finally(() => setLoading(false))
+    }, [address, onClick])
+
+    return (
+        <div className="change-account__account" onClick={handleClick}>
+            <UserAvatar className="change-account__account-avatar" address={address} small />
+            <div className="change-account__account-content">
+                <div className="change-account__account-name" title={name}>
+                    {name}
+                </div>
+                <div className="change-account__account-address">
+                    {convertAddress(address)}
+                    &nbsp;•&nbsp;
+                    {seed}
+                </div>
             </div>
-            <div className="change-account__account-address">
-                {convertAddress(address)}
-                &nbsp;•&nbsp;
-                {seed}
-            </div>
+            {loading && <Spinner />}
         </div>
-    </div>
-))
+    )
+})
