189ff92fe394edb416648c255f96fbeb0c1491ef
Egor Komarov <egor.komarov@bf.rocks>
2023-08-04 18:57:05 +0200

    feat: generate `Icons` class

---
+++ scripts/generate-icons.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+const path = require('path')
+
+const ICONS_DIR = path.resolve(__dirname, '../src/popup/assets/icons')
+const ICONS_FILE = path.resolve(__dirname, '../src/popup/icons.tsx')
+
+function capitalize(value) {
+    return value.charAt(0).toUpperCase() + value.slice(1)
+}
+
+function snakeToCamel(value) {
+    const [start, ...rest] = value.split('-')
+    return start + rest.map(capitalize).join('')
+}
+
+const files = fs.readdirSync(ICONS_DIR)
+const icons = files.map((file) => {
+    const name = snakeToCamel(file.replace('.svg', ''))
+    const component = capitalize(name)
+
+    return {
+        file,
+        name,
+        component,
+    }
+})
+
+const generated = `/* eslint-disable */
+import type { FC, SVGProps, ReactNode } from 'react'
+${icons.map(({ component, file }) => `import ${component} from '@app/popup/assets/icons/${file}'`).join('\n')}
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+export class Icons {
+${icons.map(({ name, component }) => `
+    public static readonly ${component}: FC<SVGProps<SVGSVGElement>> = ${component}
+    public static readonly ${name}: ReactNode = <${component} />
+`).join('')}
+}
+`
+
+fs.writeFile(ICONS_FILE, generated, { flag: 'w' }, (err) => {
+    if (err) throw err
+    console.log('DONE!')
+})
---
+++ src/popup/icons.tsx
@@ -0,0 +1,207 @@
+/* eslint-disable */
+import type { FC, SVGProps, ReactNode } from 'react'
+import AddUser from '@app/popup/assets/icons/add-user.svg'
+import ArrowDown from '@app/popup/assets/icons/arrow-down.svg'
+import ArrowLeft from '@app/popup/assets/icons/arrow-left.svg'
+import ArrowRight from '@app/popup/assets/icons/arrow-right.svg'
+import ArrowUp from '@app/popup/assets/icons/arrow-up.svg'
+import Card from '@app/popup/assets/icons/card.svg'
+import CheckCircle from '@app/popup/assets/icons/check-circle.svg'
+import Check from '@app/popup/assets/icons/check.svg'
+import CheckboxActive from '@app/popup/assets/icons/checkbox-active.svg'
+import ChevronDown from '@app/popup/assets/icons/chevron-down.svg'
+import ChevronLeft from '@app/popup/assets/icons/chevron-left.svg'
+import ChevronRight from '@app/popup/assets/icons/chevron-right.svg'
+import ChevronSmall from '@app/popup/assets/icons/chevron-small.svg'
+import ChevronUp from '@app/popup/assets/icons/chevron-up.svg'
+import Copy from '@app/popup/assets/icons/copy.svg'
+import CrossCircle from '@app/popup/assets/icons/cross-circle.svg'
+import Cross from '@app/popup/assets/icons/cross.svg'
+import Currency from '@app/popup/assets/icons/currency.svg'
+import Delete from '@app/popup/assets/icons/delete.svg'
+import Dots from '@app/popup/assets/icons/dots.svg'
+import Edit from '@app/popup/assets/icons/edit.svg'
+import External from '@app/popup/assets/icons/external.svg'
+import EyeOff from '@app/popup/assets/icons/eye-off.svg'
+import Eye from '@app/popup/assets/icons/eye.svg'
+import Key from '@app/popup/assets/icons/key.svg'
+import Ledger from '@app/popup/assets/icons/ledger.svg'
+import LeftArrowCircle from '@app/popup/assets/icons/left-arrow-circle.svg'
+import Loader from '@app/popup/assets/icons/loader.svg'
+import Lock from '@app/popup/assets/icons/lock.svg'
+import LogoCircle from '@app/popup/assets/icons/logo-circle.svg'
+import Logout from '@app/popup/assets/icons/logout.svg'
+import Menu from '@app/popup/assets/icons/menu.svg'
+import Minus from '@app/popup/assets/icons/minus.svg'
+import Person from '@app/popup/assets/icons/person.svg'
+import Planet from '@app/popup/assets/icons/planet.svg'
+import Plus from '@app/popup/assets/icons/plus.svg'
+import Profile from '@app/popup/assets/icons/profile.svg'
+import Recieve from '@app/popup/assets/icons/recieve.svg'
+import Refresh from '@app/popup/assets/icons/refresh.svg'
+import RightArrowCircle from '@app/popup/assets/icons/right-arrow-circle.svg'
+import Search from '@app/popup/assets/icons/search.svg'
+import Settings from '@app/popup/assets/icons/settings.svg'
+import SnackNotification from '@app/popup/assets/icons/snack-notification.svg'
+import SnackSuccess from '@app/popup/assets/icons/snack-success.svg'
+import SnackWarning from '@app/popup/assets/icons/snack-warning.svg'
+import Spinner from '@app/popup/assets/icons/spinner.svg'
+import Stake from '@app/popup/assets/icons/stake.svg'
+import Users from '@app/popup/assets/icons/users.svg'
+import Verify from '@app/popup/assets/icons/verify.svg'
+import WalletType from '@app/popup/assets/icons/wallet-type.svg'
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+export class Icons {
+
+    public static readonly AddUser: FC<SVGProps<SVGSVGElement>> = AddUser
+    public static readonly addUser: ReactNode = <AddUser />
+
+    public static readonly ArrowDown: FC<SVGProps<SVGSVGElement>> = ArrowDown
+    public static readonly arrowDown: ReactNode = <ArrowDown />
+
+    public static readonly ArrowLeft: FC<SVGProps<SVGSVGElement>> = ArrowLeft
+    public static readonly arrowLeft: ReactNode = <ArrowLeft />
+
+    public static readonly ArrowRight: FC<SVGProps<SVGSVGElement>> = ArrowRight
+    public static readonly arrowRight: ReactNode = <ArrowRight />
+
+    public static readonly ArrowUp: FC<SVGProps<SVGSVGElement>> = ArrowUp
+    public static readonly arrowUp: ReactNode = <ArrowUp />
+
+    public static readonly Card: FC<SVGProps<SVGSVGElement>> = Card
+    public static readonly card: ReactNode = <Card />
+
+    public static readonly CheckCircle: FC<SVGProps<SVGSVGElement>> = CheckCircle
+    public static readonly checkCircle: ReactNode = <CheckCircle />
+
+    public static readonly Check: FC<SVGProps<SVGSVGElement>> = Check
+    public static readonly check: ReactNode = <Check />
+
+    public static readonly CheckboxActive: FC<SVGProps<SVGSVGElement>> = CheckboxActive
+    public static readonly checkboxActive: ReactNode = <CheckboxActive />
+
+    public static readonly ChevronDown: FC<SVGProps<SVGSVGElement>> = ChevronDown
+    public static readonly chevronDown: ReactNode = <ChevronDown />
+
+    public static readonly ChevronLeft: FC<SVGProps<SVGSVGElement>> = ChevronLeft
+    public static readonly chevronLeft: ReactNode = <ChevronLeft />
+
+    public static readonly ChevronRight: FC<SVGProps<SVGSVGElement>> = ChevronRight
+    public static readonly chevronRight: ReactNode = <ChevronRight />
+
+    public static readonly ChevronSmall: FC<SVGProps<SVGSVGElement>> = ChevronSmall
+    public static readonly chevronSmall: ReactNode = <ChevronSmall />
+
+    public static readonly ChevronUp: FC<SVGProps<SVGSVGElement>> = ChevronUp
+    public static readonly chevronUp: ReactNode = <ChevronUp />
+
+    public static readonly Copy: FC<SVGProps<SVGSVGElement>> = Copy
+    public static readonly copy: ReactNode = <Copy />
+
+    public static readonly CrossCircle: FC<SVGProps<SVGSVGElement>> = CrossCircle
+    public static readonly crossCircle: ReactNode = <CrossCircle />
+
+    public static readonly Cross: FC<SVGProps<SVGSVGElement>> = Cross
+    public static readonly cross: ReactNode = <Cross />
+
+    public static readonly Currency: FC<SVGProps<SVGSVGElement>> = Currency
+    public static readonly currency: ReactNode = <Currency />
+
+    public static readonly Delete: FC<SVGProps<SVGSVGElement>> = Delete
+    public static readonly delete: ReactNode = <Delete />
+
+    public static readonly Dots: FC<SVGProps<SVGSVGElement>> = Dots
+    public static readonly dots: ReactNode = <Dots />
+
+    public static readonly Edit: FC<SVGProps<SVGSVGElement>> = Edit
+    public static readonly edit: ReactNode = <Edit />
+
+    public static readonly External: FC<SVGProps<SVGSVGElement>> = External
+    public static readonly external: ReactNode = <External />
+
+    public static readonly EyeOff: FC<SVGProps<SVGSVGElement>> = EyeOff
+    public static readonly eyeOff: ReactNode = <EyeOff />
+
+    public static readonly Eye: FC<SVGProps<SVGSVGElement>> = Eye
+    public static readonly eye: ReactNode = <Eye />
+
+    public static readonly Key: FC<SVGProps<SVGSVGElement>> = Key
+    public static readonly key: ReactNode = <Key />
+
+    public static readonly Ledger: FC<SVGProps<SVGSVGElement>> = Ledger
+    public static readonly ledger: ReactNode = <Ledger />
+
+    public static readonly LeftArrowCircle: FC<SVGProps<SVGSVGElement>> = LeftArrowCircle
+    public static readonly leftArrowCircle: ReactNode = <LeftArrowCircle />
+
+    public static readonly Loader: FC<SVGProps<SVGSVGElement>> = Loader
+    public static readonly loader: ReactNode = <Loader />
+
+    public static readonly Lock: FC<SVGProps<SVGSVGElement>> = Lock
+    public static readonly lock: ReactNode = <Lock />
+
+    public static readonly LogoCircle: FC<SVGProps<SVGSVGElement>> = LogoCircle
+    public static readonly logoCircle: ReactNode = <LogoCircle />
+
+    public static readonly Logout: FC<SVGProps<SVGSVGElement>> = Logout
+    public static readonly logout: ReactNode = <Logout />
+
+    public static readonly Menu: FC<SVGProps<SVGSVGElement>> = Menu
+    public static readonly menu: ReactNode = <Menu />
+
+    public static readonly Minus: FC<SVGProps<SVGSVGElement>> = Minus
+    public static readonly minus: ReactNode = <Minus />
+
+    public static readonly Person: FC<SVGProps<SVGSVGElement>> = Person
+    public static readonly person: ReactNode = <Person />
+
+    public static readonly Planet: FC<SVGProps<SVGSVGElement>> = Planet
+    public static readonly planet: ReactNode = <Planet />
+
+    public static readonly Plus: FC<SVGProps<SVGSVGElement>> = Plus
+    public static readonly plus: ReactNode = <Plus />
+
+    public static readonly Profile: FC<SVGProps<SVGSVGElement>> = Profile
+    public static readonly profile: ReactNode = <Profile />
+
+    public static readonly Recieve: FC<SVGProps<SVGSVGElement>> = Recieve
+    public static readonly recieve: ReactNode = <Recieve />
+
+    public static readonly Refresh: FC<SVGProps<SVGSVGElement>> = Refresh
+    public static readonly refresh: ReactNode = <Refresh />
+
+    public static readonly RightArrowCircle: FC<SVGProps<SVGSVGElement>> = RightArrowCircle
+    public static readonly rightArrowCircle: ReactNode = <RightArrowCircle />
+
+    public static readonly Search: FC<SVGProps<SVGSVGElement>> = Search
+    public static readonly search: ReactNode = <Search />
+
+    public static readonly Settings: FC<SVGProps<SVGSVGElement>> = Settings
+    public static readonly settings: ReactNode = <Settings />
+
+    public static readonly SnackNotification: FC<SVGProps<SVGSVGElement>> = SnackNotification
+    public static readonly snackNotification: ReactNode = <SnackNotification />
+
+    public static readonly SnackSuccess: FC<SVGProps<SVGSVGElement>> = SnackSuccess
+    public static readonly snackSuccess: ReactNode = <SnackSuccess />
+
+    public static readonly SnackWarning: FC<SVGProps<SVGSVGElement>> = SnackWarning
+    public static readonly snackWarning: ReactNode = <SnackWarning />
+
+    public static readonly Spinner: FC<SVGProps<SVGSVGElement>> = Spinner
+    public static readonly spinner: ReactNode = <Spinner />
+
+    public static readonly Stake: FC<SVGProps<SVGSVGElement>> = Stake
+    public static readonly stake: ReactNode = <Stake />
+
+    public static readonly Users: FC<SVGProps<SVGSVGElement>> = Users
+    public static readonly users: ReactNode = <Users />
+
+    public static readonly Verify: FC<SVGProps<SVGSVGElement>> = Verify
+    public static readonly verify: ReactNode = <Verify />
+
+    public static readonly WalletType: FC<SVGProps<SVGSVGElement>> = WalletType
+    public static readonly walletType: ReactNode = <WalletType />
+
+}
