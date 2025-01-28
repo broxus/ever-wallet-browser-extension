eb1183c0e3a40a1c99532daf7782f4c4dc50633a
Pavel Ivanov <sudokey@gmail.com>
2025-01-28 17:12:11 +0300

    fix covers

--- src/popup/modules/shared/utils/getNetworkName.ts
+++ src/popup/modules/shared/utils/getNetworkName.ts
@@ -4,6 +4,7 @@ export const getClassNameByNetwork = (connectionId: number) => {
     if (connectionId === NETWORK_ID.VENOM) return 'venom'
     if (connectionId === NETWORK_ID.EVERSCALE) return 'everscale'
     if (connectionId === NETWORK_ID.TYCHO_TESTNET) return 'tycho'
+    if (connectionId === NETWORK_ID.TON) return 'ton'
 
     return 'custom'
 }
