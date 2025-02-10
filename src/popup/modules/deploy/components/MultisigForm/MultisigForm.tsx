1befcd69af73635baebe6a178e8fea7d96669161
Pavel Ivanov <sudokey@gmail.com>
2025-02-10 10:54:12 +0000

    SXEXT-76 : Default values

--- src/popup/modules/deploy/components/MultisigForm/MultisigForm.tsx
+++ src/popup/modules/deploy/components/MultisigForm/MultisigForm.tsx
@@ -49,8 +49,8 @@ export const MultisigForm = memo(({ data, contractType, onSubmit }: Props): JSX.
     const { register, handleSubmit, formState, control, setValue } = useForm<FormValue>({
         defaultValues: useMemo(
             () => ({
-                custodians: data?.custodians.map((value) => ({ value })) ?? [{ value: '' }, { value: '' }],
-                reqConfirms: data?.reqConfirms ?? 1,
+                custodians: data?.custodians.map((value) => ({ value })) ?? [{ value: '' }, { value: '' },{ value: '' }],
+                reqConfirms: data?.reqConfirms ?? 3,
                 expirationTime: Number.isInteger(data?.expirationTime) ? data?.expirationTime : 24,
             }),
             [data],
