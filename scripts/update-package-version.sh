4d4ac7b46c53c1b0548eb0fe760adc5c72863947
Pavel Ivanov <sudokey@gmail.com>
2025-03-24 11:12:26 +0300

    bun image, release fixs

--- scripts/update-package-version.sh
+++ scripts/update-package-version.sh
@@ -14,5 +14,10 @@ increment_patch_version() {
 new_version=$(increment_patch_version $current_version)
 
 jq --arg new_version "$new_version" '.version = $new_version' package.json > package.tmp.json && mv package.tmp.json package.json
+jq --arg new_version "$new_version" '.version = $new_version' src/manifest/base.json > src/manifest/base.tmp.json && mv src/manifest/base.tmp.json src/manifest/base.json
+
+git add package.json
+git commit -m "$new_version"
+git push origin $CI_COMMIT_REF_NAME
 
 echo "Updated package version to $new_version"
